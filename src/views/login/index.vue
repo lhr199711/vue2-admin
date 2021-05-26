da6da2491f05e1a8ff6648c0c13906389f5f9a38
harry <1066521901@qq.com>
2021-05-26 22:54:04 +0800

    change

---
+++ src/views/login/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="wraper">
+    <el-button type="primary">主要按钮</el-button>
+    <el-button type="success">成功按钮</el-button>
+    <el-button type="info">信息按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.wraper {
+  background: red;
+}
+</style>
